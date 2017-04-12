3e1d0816c2872d9cc1cb5b5b580add03112cab5a
David Zuñiga <dczunigap@outlook.com>
2017-04-12 09:38:54 -0500

    - Se elimina la libreria de sqlServer y se agrega la de postgresql. - Se agrega soporte para uso de liquibase desde ant.

--- src/main/java/com/example/SpringbootappApplication.java
+++ src/main/java/com/example/SpringbootappApplication.java
@@ -2,10 +2,7 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
-//@Configuration
-/*@ComponentScan(value={"com.example.config"})*/
 @SpringBootApplication
 public class SpringbootappApplication {
 
